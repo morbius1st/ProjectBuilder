76f8909a30e7de717724c710ed596f52009d806b
morbius1st <morbius1st@gmail.com>
2017-01-01 16:51:44 -0800

    Update

---
+++ ProjectBuilder/IPDInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ProjectBuilder
+{
+	public interface IPDInfo
+	{
+		string itemNumber { get; }
+		string itemDescription { get; }
+		List<FindItem> FindItems(uProject upx, int level);
+		void Sort();
+	}
+}
